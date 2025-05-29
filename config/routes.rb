d8c936b8c9676024fbb8747b098bc5a27723a295
tknospdr <david@technospider.com>
2025-05-29 18:26:39 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,56 @@
-# frozen_string_literal: true
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-Discourse::Application.routes.append do
-  scope '/admin/plugins/force-tag-group-order', constraints: AdminConstraint.new do
-    get '' => 'admin/force_tag_group_order#index'
-    put '' => 'admin/force_tag_group_order#update'
-  end
-end
+export default class ForceTagGroupOrder extends Component {
+  @service ajax;
+  @tracked tagGroups = [];
+  @tracked selectedOrder = [];
+
+  constructor() {
+    super(...arguments);
+    this.loadTagGroups();
+  }
+
+  async loadTagGroups() {
+    try {
+      const data = await this.ajax.request('/admin/plugins/force-tag-group-order');
+      this.tagGroups = data.tag_groups;
+      this.selectedOrder = data.selected_order;
+    } catch (e) {
+      console.error('[force-tag-group-order] Error loading tag groups:', e);
+    }
+  }
+
+  @action
+  updateOrder(newOrder) {
+    this.selectedOrder = newOrder;
+    this.saveOrder();
+  }
+
+  @action
+  addTagGroup(value) {
+    if (value && !this.selectedOrder.includes(value)) {
+      this.selectedOrder = [...this.selectedOrder, value];
+      this.saveOrder();
+    }
+  }
+
+  @action
+  remove(groupName) {
+    this.selectedOrder = this.selectedOrder.filter(name => name !== groupName);
+    this.saveOrder();
+  }
+
+  async saveOrder() {
+    try {
+      await this.ajax.request('/admin/plugins/force-tag-group-order', {
+        method: 'PUT',
+        data: { order: this.selectedOrder }
+      });
+    } catch (e) {
+      console.error('[force-tag-group-order] Error saving tag group order:', e);
+    }
+  }
+}
