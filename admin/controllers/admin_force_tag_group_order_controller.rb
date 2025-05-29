b0317b9275e56a4bc88a0fcd3f8333b17bb6cc60
tknospdr <david@technospider.com>
2025-05-29 18:24:10 -0400

    Update admin_force_tag_group_order_controller.rb

--- admin/controllers/admin_force_tag_group_order_controller.rb
+++ admin/controllers/admin_force_tag_group_order_controller.rb
@@ -6,13 +6,14 @@ module Admin
 
     def index
       tag_groups = TagGroup.all.pluck(:name, :id).map { |name, id| { name: name, id: id } }
-      render_json_dump(tag_groups: tag_groups, selected_order: SiteSetting.force_tag_group_order)
+      selected_order = SiteSetting.force_tag_group_order.split(',').map(&:strip).reject(&:empty?)
+      render_json_dump(tag_groups: tag_groups, selected_order: selected_order)
     end
 
     def update
       new_order = params[:order]
       if new_order.is_a?(Array) && new_order.all? { |name| name.is_a?(String) }
-        SiteSetting.force_tag_group_order = new_order
+        SiteSetting.force_tag_group_order = new_order.join(',')
         render json: success_json
       else
         render json: failed_json, status: 400
